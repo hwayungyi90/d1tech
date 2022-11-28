eefa88b3083a84492bc107914c9a12e85e65c582
jangjiheee <jihee.jang210@gmail.com>
2022-11-28 18:18:55 +0900

    css, script 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,19 @@ window.onload = function () {
   console.log(menuList);
   var menuBg = document.querySelector(".menu_background");
   var depthTwo = document.querySelectorAll(".depth2");
-  var twoList = document.querySelectorAll(".depth2 > li")
+  var twoList = document.querySelectorAll(".depth2")
   var menuWhole = document.getElementById("menu_gnb");
   var depthOne = document.querySelectorAll(".depth1");
   let clickList = document.querySelectorAll(".clickList");
-  var oneP = depthOne.parentNode;
-  console.log(oneP);
+  console.log(clickList);
+  let clickPa = clickList.parent;
+  console.log(clickPa);
 
   for (let i = 0; i < twoList.length; i++) {
     twoList[i].addEventListener("mouseenter", function () {
-      this.parentNode(depthOne).classList.add("active");
+      this.prev(depthOne).classList.add("active");
     })
   }
-
 
   menuWhole.addEventListener("mouseenter", function () {
     menuBg.style.setProperty('height', '380px');
