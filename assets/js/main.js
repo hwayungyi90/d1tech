a77c152f2b1e0c0fb9495278d95352ae076dcf26
jangjiheee <jihee.jang210@gmail.com>
2022-11-28 17:52:24 +0900

    script, 사업소개 페이지 업데이트

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,23 @@ window.onload = function () {
   var menuList = document.querySelectorAll(".first_list");
   console.log(menuList);
   var menuBg = document.querySelector(".menu_background");
-  console.log(menuBg);
   var depthTwo = document.querySelectorAll(".depth2");
-  console.log(depthTwo);
+  var twoList = document.querySelectorAll(".depth2 > li")
   var menuWhole = document.getElementById("menu_gnb");
+  var depthOne = document.querySelectorAll(".depth1");
+  let clickList = document.querySelectorAll(".clickList");
+  var oneP = depthOne.parentNode;
+  console.log(oneP);
+
+  for (let i = 0; i < twoList.length; i++) {
+    twoList[i].addEventListener("mouseenter", function () {
+      this.parentNode(depthOne).classList.add("active");
+    })
+  }
+
 
   menuWhole.addEventListener("mouseenter", function () {
-    menuBg.style.setProperty('height', '500px');
+    menuBg.style.setProperty('height', '380px');
     for (let j = 0; j < depthTwo.length; j++) {
       depthTwo[j].style.setProperty('display', 'block');
     }
@@ -20,4 +30,16 @@ window.onload = function () {
       depthTwo[j].style.setProperty('display', 'none');
     }
   })
+
+  // for (let i=0; i<clickList.length; i++) {
+  //   clickList[i].addEventListener("mouseenter", function() {
+  //     for (let j=0; j<clickList.length; j++) {
+  //       if (clickList[j] == this) {
+  //           clickList[j].classList.add("active")
+  //       } else {
+  //           clickList[j].classList.remove("active")
+  //       }
+  //     }
+  //   });
+  // }
 }
